4e907edd46cefc38a44ba7ae4f9d5e8e5434bbe0
Ethan <ethancavill@gmail.com>
2024-10-17 15:29:52 -0700

    Updates library system

--- oop_example3.py
+++ oop_example3.py
@@ -9,34 +9,62 @@
 # Track item availability and due dates
 # Generate reports for popular items, overdue items, member activity
 
-
 # Ok so it seems like we definitely will have a few classes
 # I'm wondering how we should handle inventories, because we need a total store but also a locality
 # I guess the main inventory should be centralised, we just use a attribute to discern where its from
 
+# What functions will a library manage
+# Adding a branch
+# Inventory management
+    # Moving book between branches
+    # Adding book
+    # Removing book
+# Generating reports
+
 class Library:
-    def __init__(self, name, branches=3):
+    def __init__(self, name):
         self.name = name
-        self.branches = [Branch(i, self) for i in range(branches)]    
+        self.branches = [] 
         self.inventory = {}
-        self.members = {}
-    
+        self.members = {} 
+
+# What functions will a branch manage
+# Member signs up through a branch
+# Inventory management
+    # Book checkout
+    # Book return
 
 class Branch:
-    def __init__(self, id, parent):
+    def __init__(self, id, name, parent):
         self.id = id
+        self.name = name
         self.library = parent
+
+# What functions will a member conduct
+# Or is it more of a data store?
 
 class Member:
     counter = 0
     
-    def __init__(self, name):
+    def __init__(self, name, branch):
         Member.counter += 1
         self.id = Member.counter
         self.name = name
-        
+        self.branch = branch
+        self.history = {}
+        self.checkouts = {}
+
+    def checkout(self, book_id):
+        pass
 
 class Book:
-    pass
+    counter = 0
 
-
+    def __init__(self, title, branch, *authors):
+        Book.counter += 1
+        self.id = Book.counter
+        self.title = title
+        self.branch = branch
+        self.authors = list(authors)
+        self.available = True
+        self.borrower = None
